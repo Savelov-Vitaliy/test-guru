99e339d95abbd6d0e70c1f788beb0d5a039ba240
Savelov-Vitaliy <savelov_vitaliy@mail.ru>
2018-05-24 22:53:07 +0500

    part_6

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,13 +1,16 @@
 class Test < ApplicationRecord
   belongs_to :category
   has_many :questions
-  has_many :answers
   has_many :test_passages
   has_many :users, through: :test_passages
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  def self.category(category)    
-    joins(:category).where(categories: {title: category}).order(title: :desc).pluck(:title)    
-  end
+  scope :light, -> { where(level: 0..1) }
+  scope :normal, -> { where(level: 2..4) }
+  scope :hard, -> { where(level: 5..Float::INFINITY) }
+  scope :category, -> (category) { joins(:category).where(categories: {title: category}).order(title: :desc) }
+
+  validates :title, presence: true, uniqueness: { scope: :level,  message: "Тест с такими title и level уже есть в базе" }
+  validates :level, numericality: { greater_than: 0, only_integer: true }
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_24_065135) do
+ActiveRecord::Schema.define(version: 2018_05_24_161732) do
 
   create_table "answers", force: :cascade do |t|
     t.string "body", null: false
@@ -61,6 +61,7 @@ ActiveRecord::Schema.define(version: 2018_05_24_065135) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
