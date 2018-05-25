8b0446baa324177b28faf28a6f11086eab515911
Savelov-Vitaliy <savelov_vitaliy@mail.ru>
2018-05-25 15:54:42 +0500

    part_6 correct

--- app/models/test.rb
+++ app/models/test.rb
@@ -5,12 +5,16 @@ class Test < ApplicationRecord
   has_many :users, through: :test_passages
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  scope :light, -> { where(level: 0..1) }
+  scope :easy, -> { where(level: 0..1) }
   scope :normal, -> { where(level: 2..4) }
   scope :hard, -> { where(level: 5..Float::INFINITY) }
-  scope :category, -> (category) { joins(:category).where(categories: {title: category}).order(title: :desc) }
+  scope :all_by_category, -> (category) { joins(:category).where(categories: {title: category}).order(title: :desc) }
 
   validates :title, presence: true, uniqueness: { scope: :level,  message: "Тест с такими title и level уже есть в базе" }
   validates :level, numericality: { greater_than: 0, only_integer: true }
+
+  def self.category_title(category)
+    all_by_category(category).pluck(:title) 
+  end
 
 end
