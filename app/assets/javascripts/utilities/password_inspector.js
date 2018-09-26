a9ec6e42d8fbc38cc8da6b9e8a1d849264b0c368
Savelov-Vitaliy <savelov_vitaliy@mail.ru>
2018-09-26 09:54:04 +0500

    v.1.2

--- app/assets/javascripts/utilities/password_inspector.js
+++ app/assets/javascripts/utilities/password_inspector.js
@@ -1,22 +1,22 @@
 document.addEventListener('turbolinks:load', function(){
-  var password_confirmation = document.getElementById('user_password_confirmation')
+  var password_confirmation = document.getElementById('user_password_confirmation');
 
   if (password_confirmation) { password_confirmation.addEventListener('input', password_inspector) }
 })
 
 function password_inspector() {
-  var password = document.getElementById('user_password').value
-  var password_confirmation = document.getElementById('user_password_confirmation').value
-  var alert_cL = document.querySelector('.octicon-alert').classList
-  var check_cL = document.querySelector('.octicon-check').classList
+  var password = document.getElementById('user_password').value;
+  var password_confirmation = document.getElementById('user_password_confirmation').value;
+  var alert_cL = document.querySelector('.octicon-alert').classList;
+  var check_cL = document.querySelector('.octicon-check').classList;
 
   if (password.length != 0) {
     if (password == password_confirmation) {
-      alert_cL.add('hide')
-      check_cL.remove('hide')
+      alert_cL.add('hide');
+      check_cL.remove('hide');
     } else {
-      alert_cL.remove('hide')
-      check_cL.add('hide')
+      alert_cL.remove('hide');
+      check_cL.add('hide');
     }
   }
 
