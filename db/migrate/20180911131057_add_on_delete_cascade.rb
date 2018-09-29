cbb84bf0c1b5c1347b054ca9e8682cd8b6e8c151
Savelov-Vitaliy <savelov_vitaliy@mail.ru>
2018-09-29 16:21:06 +0500

    v1.3

--- db/migrate/20180911131057_add_on_delete_cascade.rb
+++ db/migrate/20180911131057_add_on_delete_cascade.rb
@@ -1,6 +1,6 @@
 class AddOnDeleteCascade < ActiveRecord::Migration[5.2]
   def change
-    remove_foreign_key :tests, :questions
-    add_foreign_key :questions, :tests, on_delete: :cascade
+    #remove_foreign_key :tests, :questions
+    #add_foreign_key :questions, :tests, on_delete: :cascade
   end
 end
