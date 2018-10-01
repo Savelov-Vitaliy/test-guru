34ab3e530746706cee132e48c8d84d6645ed55af
Savelov-Vitaliy <savelov_vitaliy@mail.ru>
2018-10-01 17:29:40 +0500

    v1.14

--- app/models/test.rb
+++ app/models/test.rb
@@ -2,7 +2,7 @@ class Test < ApplicationRecord
   belongs_to :category
   has_many :questions, dependent: :destroy
   has_many :test_passages
-  has_many :users, through: :test_passages
+  has_many :users, through: :test_passages, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   scope :easy, -> { where(level: 0..1) }
